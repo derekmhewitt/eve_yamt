06f3e407903027ab444930cdd7c14374377ea54e
Logan Rogers <logan.s.rogers@biola.edu>
2016-08-01 14:49:54 -0700

    changed formatting in server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 
 var express = require('express'),
-	requestProxy = require('express-request-proxy'),
-	port = process.env.PORT || 4000,
-	app = express();
+  requestProxy = require('express-request-proxy'),
+  port = process.env.PORT || 4000,
+  app = express();
 
 var proxyEve = function(request, response) {
-	console.log('Routing EVE request for ', request.params[0]);
-	(
+  console.log('Routing EVE request for ', request.params[0]);
+  (
 		requestProxy({
-			url: 'http://api.eve-central.com/' + request.params[0],
+  url: 'http://api.eve-central.com/' + request.params[0],
 		})
 	)(request, response);
 };
@@ -18,10 +18,10 @@ app.get('/eve-central/*', proxyEve);
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
-	console.log('New request: ', request.url);
-	response.sendFile('index.html', {root: '.'});
+  console.log('New request: ', request.url);
+  response.sendFile('index.html', {root: '.'});
 });
 
 app.listen(port, function() {
-	console.log('Server started on port ' + port + '!');
+  console.log('Server started on port ' + port + '!');
 });
