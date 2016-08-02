c147d41757fe89cd9dcd9806f080bea01a0e80d9
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-08-02 14:26:40 -0700

    making some ajax calls for objects, handled both single and index models

--- public/scripts/model/indexModel.js
+++ public/scripts/model/indexModel.js
@@ -1,9 +1,9 @@
 (function(module) {
 
 	var indexData = {};
-	var indexObjects = [];
 
-	var dataQuery = [
+	var indexObjects = [];
+	var indexQuery = [
 		{
    	 		"idNum": "16",
    	 		"itemName": "Stargate (Caldari System)"
@@ -30,23 +30,23 @@
   		}
 	];
 
-	indexData.frontPageData = function() {
-		dataQuery.map(function(item) {
-			$.get('/eve-central/api/marketstat/json?hours=1&typeid=' + item.idNum)
+	indexData.displayDataObject = function() {
+		dataQuery.map(function(queryObject) {
+			$.get('/eve-central/api/marketstat/json?hours=1&typeid=' + queryObject.idNum)
 			.done(function(oneHourData) {
 				oneHourData.map(function(oneHour) {
-					$.get('/eve-central/api/marketstat/json?hours=2&typeid=' + item.idNum)
+					$.get('/eve-central/api/marketstat/json?hours=2&typeid=' + queryObject.idNum)
 					.done(function(twoHourData) {
 						twoHourData.map(function(twoHour) {
 							indexObjects.push({
-							name: item.itemName,
-							id: twoHour.all.forQuery.types[0],
-							buyAvg: oneHour.buy.avg.toFixed(2),
-							sellAvg: oneHour.sell.avg.toFixed(2),
-							oneHourAvg: oneHour.all.avg.toFixed(2),
-							twoHourAvg: twoHour.all.avg.toFixed(2)
+								name: 		queryObject.itemName,
+								id: 		twoHour.all.forQuery.types[0],
+								buyAvg: 	oneHour.buy.avg.toFixed(2),
+								sellAvg: 	oneHour.sell.avg.toFixed(2),
+								oneHourAvg: oneHour.all.avg.toFixed(2),
+								twoHourAvg: twoHour.all.avg.toFixed(2)
 							});
-							console.log(indexObjects);
+							// console.log(indexObjects);
 						});
 					});
 				});
