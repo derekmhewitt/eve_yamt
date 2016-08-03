6c1ae11a48037c818dddd419fa75966b11b61da8
Logan Rogers <logan.s.rogers@biola.edu>
2016-08-03 10:33:39 -0700

    added event listener to search

--- public/scripts/model/customizeModel.js
+++ public/scripts/model/customizeModel.js
@@ -3,14 +3,19 @@
   //Use Jquery to grab text box element and store it.
   //Event handler for input that searches the Firebase database and stores result.
   customizeModel.itemData = firebase.database().ref('itemData');
-
+  customizeModel.searchQuery = null;
+  customizeModel.queryResult = null;
   //needs to be a listener on this value
-  var searchQuery = $('#customize-input').value;
+  $('#customize-form').on('click', function(){
+    customizeModel.searchQuery = $('#customize-input').value;
+    customizeModel.findItem(customizeModel.searchQuery);
+  });
 
   customizeModel.findItem = function(searchQuery) {
     query = database.ref('/itemData/').orderByChild('itemName').equalTo(searchQuery).once('value').then(
       function(data) {
         console.log(data.val());
+        customizeModel.queryResult = data.val();
       }
     );
   };
