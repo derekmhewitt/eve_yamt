14c4ee2688f60438e7dcc1f76312197811d088f9
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-08-03 14:32:20 -0700

    got the front page working, yay

--- public/scripts/view/indexView.js
+++ public/scripts/view/indexView.js
@@ -1,20 +1,20 @@
 
 (function(module) {
-  	
-  	var indexView = {};
-  	var indexSource = $('#index-template').html();
-  	var indexContent = Handlebars.compile(indexSource);
-  
-  	indexView.render = function(currentObject) {tenlia
-  		if(currentObject.oneHourAvg < currentObject.twoHourAvg) {
-  			currentObject.symbol = '<h3 id="upArrow"></h3>';
-  		} else if(currentObject.oneHourAvg > currentObject.twoHourAvg) {
-  			currentObject.symbol = '<h3 id="downArrow"></h3>';
-  		} else {
-  			currentObject.symbol = '<h3 id="exclamation-circle"></h3>';
-  		}
-  		$('#home').append(indexContent(currentObject));
-	};
+
+	var indexView = {};
+	var indexSource = $('#index-template').html();
+	var indexContent = Handlebars.compile(indexSource);
+
+	indexView.render = function(currentObject) {
+		if(currentObject.oneHourAvg < currentObject.twoHourAvg) {
+			currentObject.symbol = '<h3 id="upArrow"></h3>';
+		} else if(currentObject.oneHourAvg > currentObject.twoHourAvg) {
+			currentObject.symbol = '<h3 id="downArrow"></h3>';
+		} else {
+			currentObject.symbol = '<h3 id="exclamation-circle"></h3>';
+		}
+		$('#home').append(indexContent(currentObject));
+  };
 
   	module.indexView = indexView;
 
