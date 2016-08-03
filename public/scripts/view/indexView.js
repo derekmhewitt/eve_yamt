6b445f9cbde44df859b3c61e8e22b64029792763
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-08-03 10:44:52 -0700

    got the handlebars to work for the front page. Liz&Aziza

--- public/scripts/view/indexView.js
+++ public/scripts/view/indexView.js
@@ -2,13 +2,33 @@
 (function(module) {
   	
   	var indexView = {};
-  	var indexCompiler = Handlebars.compile($('#index-template').text());
+
+
   
   	indexView.render = function() {
-    	('').empty().append();
-  	};
+	  	var indexSource = $('#index-template').html();
+	  	var indexContent = Handlebars.compile(indexSource);
+	  	console.log(indexObjects);
 
+	  	indexObjects.map(function(currentObject) {
+	  		if(currentObject.oneHourAvg < currentObject.twoHourAvg) {
+	  			currentObject.symbol = '<h3 id="upArrow"></h3>';
+	  		} else if(currentObject.oneHourAvg > currentObject.twoHourAvg) {
+	  			currentObject.symbol = '<h3 id="downArrow"></h3>';
+	  		} else {
+	  			currentObject.symbol = '<h3 id="exclamation-circle"></h3>';
+	  		}
+	  		$('#home').append(indexContent(currentObject));
+	  	});
+	  	// .map(function(currentEntry) {
+	  	// 	console.log('current entry appended', currentEntry);
+	  	// });
+	};
+
+	indexView.render();
+   //  	('#home').append(indexTemplate);
   	// indexObj.request(indexView,render);
+
   	module.indexView = indexView;
 
 })(window);
